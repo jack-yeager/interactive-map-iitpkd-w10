414c529b251246a091e71bc768371b886eacecb6
mummidiyaswant <134735085+mummidiyaswant@users.noreply.github.com>
2023-05-31 16:01:59 +0530

    Update h3.py

--- h3.py
+++ h3.py
@@ -7,9 +7,10 @@ map= folium.Map(location=[10.803489489100986, 76.81870306540125],zoom_start=15)#
 
 html= '<img src= "data:image/png;base64,{}">'.format#formating all image files to be added for html
 
-#for location 1: Taste of malabar
+#for location : h3
 picture_h3= base64.b64encode(open("./tom.jpg",'rb').read()).decode()#encoding and decoding the image filr
 iframe_h3= IFrame(html(picture_h3),width=327+20,height=160+20)#IFrame used to add picture in popup
 popup_h3= folium.Popup(iframe_h3,max_width=650)
-ricon= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
+ricon= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized hostel icon
 folium.Marker(location=[10.797984370712905, 76.81832584827522],tooltip="<strong>Taste of Malabar</strong>",popup=popup_h3,icon=ricon).add_to(map)#for displaying everything in the given icon
+map.save("h3.html")
