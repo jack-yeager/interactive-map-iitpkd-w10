0585c4cb87f74f36901ea85d10bd8b30c48c8f33
mummidiyaswant <134735085+mummidiyaswant@users.noreply.github.com>
2023-05-31 16:39:23 +0530

    Update h1.py

--- h1.py
+++ h1.py
@@ -11,5 +11,5 @@ picture_h1= base64.b64encode(open("./tom.jpg",'rb').read()).decode()#encoding an
 iframe_h1= IFrame(html(picture_h1),width=327+20,height=160+20)#IFrame used to add picture in popup
 popup_h1= folium.Popup(iframe_h1,max_width=650)
 ricon_h1= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
-folium.Marker(location=[10.799134430915721, 76.81813182028293],tooltip="<strong>Taste of Malabar</strong>",popup=popup_h1,icon=ricon_h1).add_to(map)#for displaying everything in the given icon
+folium.Marker(location=[10.799205621030923, 76.82223021773736],tooltip="<strong>Hostel 1</strong>",popup=popup_h1,icon=ricon_h1).add_to(map)#for displaying everything in the given icon
 map.save("h1.html")
