033e3f6854995788011b7979ab1213a14fc766ed
mummidiyaswant <134735085+mummidiyaswant@users.noreply.github.com>
2023-05-31 16:11:19 +0530

    Update h2.py

--- h2.py
+++ h2.py
@@ -11,5 +11,5 @@ picture_h2= base64.b64encode(open(,'rb').read()).decode()#encoding and decoding 
 iframe_h2= IFrame(html(picture_h2),width=327+20,height=160+20)#IFrame used to add picture in popup
 popup_h2= folium.Popup(iframe_h2,max_width=650)
 ricon_h2= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
-folium.Marker(location=[10.799134430915721, 76.81813182028293],tooltip="<strong>Taste of Malabar</strong>",popup=popup_h2,icon=ricon_h2).add_to(map)#for displaying everything in the given icon
+folium.Marker(location=[10.799550455187008, 76.8222669062808],tooltip="<strong>Taste of Malabar</strong>",popup=popup_h2,icon=ricon_h2).add_to(map)#for displaying everything in the given icon
 map.save("h2.html")
