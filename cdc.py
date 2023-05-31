dbf3d4862a000d6aa31718eda57046ed69c60e81
mummidiyaswant <134735085+mummidiyaswant@users.noreply.github.com>
2023-05-31 16:30:09 +0530

    Update cdc.py

--- cdc.py
+++ cdc.py
@@ -1,1 +1,15 @@
+import folium
+from folium import IFrame
+import base64
 
+map= folium.Map(location=[10.803489489100986, 76.81870306540125],zoom_start=15)#opens the map
+
+html= '<img src= "data:image/png;base64,{}">'.format#formating all image files to be added for html
+
+#for location 1: Taste of malabar
+picture_cdc= base64.b64encode(open(,'rb').read()).decode()#encoding and decoding the image filr
+iframe_cdc= IFrame(html(picture_cdc),width=327+20,height=160+20)#IFrame used to add picture in popup
+popup_cdc= folium.Popup(iframe_cdc,max_width=650)
+ricon_cdc= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
+folium.Marker(location=[10.80002997087941, 76.81874784806095],tooltip="<strong>Taste of Malabar</strong>",popup=popup_cdc,icon=ricon_cdc).add_to(map)#for displaying everything in the given icon
+map.save("cdc.html")
