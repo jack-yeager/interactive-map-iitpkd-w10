9ad8e947712408b8f8215fbedd080eedad416d26
mummidiyaswant <134735085+mummidiyaswant@users.noreply.github.com>
2023-05-31 16:47:37 +0530

    Update h3.py

--- h3.py
+++ h3.py
@@ -12,5 +12,5 @@ picture_h3= base64.b64encode(open("./tom.jpg",'rb').read()).decode()#encoding an
 iframe_h3= IFrame(html(picture_h3),width=327+20,height=160+20)#IFrame used to add picture in popup
 popup_h3= folium.Popup(iframe_h3,max_width=650)
 ricon= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized hostel icon
-folium.Marker(location=[10.797984370712905, 76.81832584827522],tooltip="<strong>Taste of Malabar</strong>",popup=popup_h3,icon=ricon).add_to(map)#for displaying everything in the given icon
+folium.Marker(location=[10.797984370712905, 76.81832584827522],tooltip="<strong>Hostel 3</strong>",popup=popup_h3,icon=ricon).add_to(map)#for displaying everything in the given icon
 map.save("h3.html")
