e037052ec80f7ad19393f57d0daa8165402e633c
mummidiyaswant <134735085+mummidiyaswant@users.noreply.github.com>
2023-05-31 16:48:26 +0530

    Update cdc.py

--- cdc.py
+++ cdc.py
@@ -11,5 +11,5 @@ picture_cdc= base64.b64encode(open(,'rb').read()).decode()#encoding and decoding
 iframe_cdc= IFrame(html(picture_cdc),width=327+20,height=160+20)#IFrame used to add picture in popup
 popup_cdc= folium.Popup(iframe_cdc,max_width=650)
 ricon_cdc= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
-folium.Marker(location=[10.80002997087941, 76.81874784806095],tooltip="<strong>Taste of Malabar</strong>",popup=popup_cdc,icon=ricon_cdc).add_to(map)#for displaying everything in the given icon
+folium.Marker(location=[10.80002997087941, 76.81874784806095],tooltip="<strong>Cafe Day Cafeteria</strong>",popup=popup_cdc,icon=ricon_cdc).add_to(map)#for displaying everything in the given icon
 map.save("cdc.html")
