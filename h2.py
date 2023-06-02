7ad45f6823c2c69cc7812ce2c9d5ea42bf8ab8ba
mummidiyaswant <134735085+mummidiyaswant@users.noreply.github.com>
2023-06-02 17:35:48 +0530

    Update h2.py

--- h2.py
+++ h2.py
@@ -1,1 +1,15 @@
+import folium
+from folium import IFrame
+import base64
 
+map= folium.Map(location=[10.803489489100986, 76.81870306540125],zoom_start=15)#opens the map
+
+html= '<img src= "data:image/png;base64,{}">'.format#formating all image files to be added for html
+
+#for location 1: Taste of malabar
+picture_h2= base64.b64encode(open(,'rb').read()).decode()#encoding and decoding the image filr
+iframe_h2= IFrame(html(picture_h2),width=327+20,height=160+20)#IFrame used to add picture in popup
+popup_h2= folium.Popup(iframe_h2,max_width=650)
+ricon_h2= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
+folium.Marker(location=[10.799550455187008, 76.8222669062808],tooltip="<strong>Hostel 2</strong>",popup=popup_h2,icon=ricon_h2).add_to(map)#for displaying everything in the given icon
+map.save("h2.html")
