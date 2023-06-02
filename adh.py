a5a0bf03901686bb6cfe71467dd82641540cf0e0
akkisai <134312204+akkisai@users.noreply.github.com>
2023-06-02 17:27:31 +0530

    Update adh.py

--- adh.py
+++ adh.py
@@ -5,10 +5,10 @@ import base64
 map= folium.Map(location=[10.803489489100986, 76.81870306540125],zoom_start=15)#opens the map need
 
 html= '<img src= "data:image/png;base64,{}">'.format
-picture7= base64.b64encode(open("./adh.jpg",'rb').read()).decode()
-iframe7= IFrame(html(picture7),width=327+20,height=225+20)
-popup7= folium.Popup(iframe7,max_width=640)
-ricon7= folium.features.CustomIcon('hosp.png',icon_size=(40,40))
-folium.Marker(location=[10.792077430340598, 76.82749318189833],tooltip="<strong>Ahalia Diabetic Hospital</strong>",popup=popup7,icon=ricon7).add_to(map) 
+picture_adh= base64.b64encode(open(,'rb').read()).decode()
+iframe_adh= IFrame(html(picture_adh),width=327+20,height=225+20)
+popup_adh= folium.Popup(iframe_adh,max_width=640)
+ricon_adh= folium.features.CustomIcon('hosp.png',icon_size=(40,40))
+folium.Marker(location=[10.79221717547408, 76.82741136465046],tooltip="<strong>Ahalia Diabetic Hospital</strong>",popup=popup_adh,icon=ricon_adh).add_to(map) 
 
 map.save("adh.html")
