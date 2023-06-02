30359f80a038a45f82ea471f8d66ce9e21adc872
akkisai <134312204+akkisai@users.noreply.github.com>
2023-06-02 23:07:58 +0530

    Update staff.py

--- staff.py
+++ staff.py
@@ -11,5 +11,5 @@ picture_st= base64.b64encode(open(,'rb').read()).decode()#encoding and decoding 
 iframe_st= IFrame(html(picture_st),width=327+20,height=160+20)#IFrame used to add picture in popup
 popup_st= folium.Popup(iframe_st,max_width=650)
 ricon_st= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
-folium.Marker(location=[10.8017955754564, 76.81849606364607],tooltip="<strong>Staff Quaters</strong>",popup=popup_st,icon=ricon_st).add_to(map)#for displaying everything in the given icon
+folium.Marker(location=[10.797701841955542, 76.81851095051009],tooltip="<strong>Staff Quaters</strong>",popup=popup_st,icon=ricon_st).add_to(map)#for displaying everything in the given icon
 map.save("st.html")
