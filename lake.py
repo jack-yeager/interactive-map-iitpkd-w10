7d1401901e062e7d48a10a3fc3531ccada0fe032
akkisai <134312204+akkisai@users.noreply.github.com>
2023-06-08 18:31:30 +0530

    Update lake.py

--- lake.py
+++ lake.py
@@ -2,14 +2,16 @@ import folium
 from folium import IFrame
 import base64
 
-map= folium.Map(location=[10.803489489100986, 76.81870306540125],zoom_start=15)#opens the map
+map = folium.Map(location=[10.796019890314097,
+                 76.82275843702458], zoom_start=16)
 
-html= '<img src= "data:image/png;base64,{}">'.format#formating all image files to be added for html
+html = '<img src= "data:image/png;base64,{}">'.format
 
-#for location 1: Taste of malabar
-picture_l= base64.b64encode(open("./tom.jpg",'rb').read()).decode()#encoding and decoding the image filr
-iframe_l= IFrame(html(picture_l),width=327+20,height=160+20)#IFrame used to add picture in popup
-popup_l= folium.Popup(iframe_l,max_width=650)
-ricon_l= folium.features.CustomIcon('img.png',icon_size=(40,40))#customized food icon
-folium.Marker(location=[10.794969103780806, 76.82528481763399],tooltip="<strong>Lake park</strong>",popup=popup_l,icon=ricon_l).add_to(map)#for displaying everything in the given icon
+picture_l = base64.b64encode(open("./lake.jpg", 'rb').read()).decode()
+iframe_l = IFrame(html(picture_l), width=327+20, height=273+20)
+popup_l = folium.Popup(iframe_l, max_width=650)
+ricon_l = folium.features.CustomIcon('la.png', icon_size=(60, 60))
+folium.Marker(location=[10.794969103780806, 76.82528481763399],
+              tooltip="<strong>Lake park</strong>", popup=popup_l, icon=ricon_l).add_to(map)
+
 map.save("lake.html")
