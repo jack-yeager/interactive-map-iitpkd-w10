f8c5a6d6fcb535ce67d7df0da4fef182fddc93ab
jack-yeager <132974701+jack-yeager@users.noreply.github.com>
2023-06-08 20:51:25 +0530

    Update basketball.py

--- basketball.py
+++ basketball.py
@@ -2,12 +2,16 @@ import folium
 from folium import IFrame
 import base64
 
-map= folium.Map(location=[10.803489489100986, 76.81870306540125],zoom_start=15)#opens the map need
+map = folium.Map(location=[10.796019890314097,
+                 76.82275843702458], zoom_start=16)
 
-html= '<img src= "data:image/png;base64,{}">'.format
-picture_bk= base64.b64encode(open(,'rb').read()).decode()
-iframe_bk= IFrame(html(picture_bk),width=327+20,height=225+20)
-popup_bk= folium.Popup(iframe_bk,max_width=640)
-ricon_bk= folium.features.CustomIcon('hosp.png',icon_size=(40,40))
-folium.Marker(location=[10.803404533363029, 76.8187337141756],tooltip="<strong>Basketball Court</strong>",popup=popup_bk,icon=ricon_bk).add_to(map) 
-map.save("bk.html")
+html = '<img src= "data:image/png;base64,{}">'.format
+
+picture_bk = base64.b64encode(open('basketball.jpg', 'rb').read()).decode()
+iframe_bk = IFrame(html(picture_bk), width=327+20, height=245+20)
+popup_bk = folium.Popup(iframe_bk, max_width=640)
+ricon_bk = folium.features.CustomIcon('bb.png', icon_size=(40, 40))
+folium.Marker(location=[10.802793275868506, 76.81854055985796],
+              tooltip="<strong>Basketball Court</strong>", popup=popup_bk, icon=ricon_bk).add_to(map)
+
+map.save("bb.html")
